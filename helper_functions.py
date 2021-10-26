040a54798bbf837b144cfb3a8cffb0a9cd1eb270
Dylan Budnick <dylanjbudnick@gmail.com>
2021-10-25 21:06:14 -0700

    adding lambdata package and helper_function.py file

---
+++ helper_functions.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np
+
+def null_count(df):
+    '''Counts the number of null values 
+    across the entire dataframe'''
+    return df.isnull().sum().sum()
+
+
+def list_2_series(list_2_series, df):
+    '''Takes a list and dataframe then turns the list into a series 
+    and adds it to the inputted dataframe as a new column'''
+    return df['List'] = list_2_series
+
